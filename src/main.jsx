60e2372284efb01b21b4b80b8d3cbb7148ce4aab
Kedar <kgurav678@gmail.com>
2026-01-10 23:21:16 +0530

    Remove i18next implementation - using Google Translate instead

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import './i18n'
 
 // Enhanced Error Boundary with recovery for Google Translate DOM conflicts
 class ErrorBoundary extends React.Component {
